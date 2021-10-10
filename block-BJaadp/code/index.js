339e12edf35c784f1b9baecc837a756fae42fe4e
Jaidutta <dutta_joy@hotmail.com>
2021-10-10 21:55:38 +0100

    completes Array & String Methods - Practice Reduce Methods - Block 2

--- block-BJaadp/code/index.js
+++ block-BJaadp/code/index.js
@@ -1,46 +1,109 @@
 let persons = [
-  { name: 'John', grade: 8, sex: 'M' },
-  { name: 'Sarah', grade: 12, sex: 'F' },
-  { name: 'Bob', grade: 16, sex: 'M' },
-  { name: 'Johnny', grade: 2, sex: 'M' },
-  { name: 'Ethan', grade: 4, sex: 'M' },
-  { name: 'Paula', grade: 18, sex: 'F' },
-  { name: 'Donald', grade: 5, sex: 'M' },
-  { name: 'Jennifer', grade: 13, sex: 'F' },
-  { name: 'Courtney', grade: 15, sex: 'F' },
-  { name: 'Jane', grade: 9, sex: 'F' },
-  { name: 'John', grade: 17, sex: 'M' },
-  { name: 'Arya', grade: 14, sex: 'F' },
+  { name: "John", grade: 8, sex: "M" },
+  { name: "Sarah", grade: 12, sex: "F" },
+  { name: "Bob", grade: 16, sex: "M" },
+  { name: "Johnny", grade: 2, sex: "M" },
+  { name: "Ethan", grade: 4, sex: "M" },
+  { name: "Paula", grade: 18, sex: "F" },
+  { name: "Donald", grade: 5, sex: "M" },
+  { name: "Jennifer", grade: 13, sex: "F" },
+  { name: "Courtney", grade: 15, sex: "F" },
+  { name: "Jane", grade: 9, sex: "F" },
+  { name: "John", grade: 17, sex: "M" },
+  { name: "Arya", grade: 14, sex: "F" },
 ];
 
 // NOTE: Use reduce method whereever you can to solve this exercise:
 
 // Find the average grade
+const totalGrades = persons.reduce((acc, currentPerson) => {
+  acc = acc + currentPerson.grade;
+  return acc;
+}, 0);
+
+const avgGrade = totalGrades / persons.length;
 
 // Find the average grade of male
+let maleArr = persons.reduce((acc, cV) => {
+  if (cV.sex === "M") {
+    acc.push(cV.grade);
+  }
+
+  return acc;
+}, []);
+
+let totalMales = maleArr.length;
+let totalMaleGrade = maleArr.reduce((acc, currentGrade) => {
+  acc = acc + currentGrade;
+  return acc;
+}, 0);
+
+const avgMaleGrade = totalMaleGrade / totalMales;
 
 // Find the average grade of female
+const femaleGrades = persons.reduce((acc, currentPerson) => {
+  if (currentPerson.sex === "F") {
+    acc.push(currentPerson.grade);
+    // console.log(acc, currentPerson.grade);
+  }
+  return acc;
+}, []);
+
+const totalFemaleGrade = femaleGrades.reduce((acc, currentFemaleGrade) => {
+  acc = acc + currentFemaleGrade;
+  return acc;
+}, 0);
+
+const avgFemaleGrade = totalFemaleGrade / femaleGrades.length;
 
 // Find the highest grade
+const hightestGrade = persons.reduce((acc, currentPerson) => {
+  if (currentPerson.grade > acc) {
+    acc = currentPerson.grade;
+  }
+  return acc;
+}, 0);
 
 // Find the highest grade in male
+const highestGradeMale = persons.reduce((acc, currentPerson) => {
+  if ((currentPerson.grade > acc) & (currentPerson.sex === "M")) {
+    acc = currentPerson.grade;
+  }
+  return acc;
+}, 0);
 
 // Find the highest grade in female
+const highestGradeFemale = persons.reduce((acc, currentPerson) => {
+  if ((currentPerson.grade > acc) & (currentPerson.sex === "F")) {
+    acc = currentPerson.grade;
+  }
+  return acc;
+}, 0);
 
 // Find the highest grade for people whose name starts with 'J' or 'P'
+const specialPeopleWithHigestGrade = persons.reduce((acc, currentPerson) => {
+  if (
+    (currentPerson.name.startsWith("J") ||
+      currentPerson.name.startsWith("P")) &&
+    currentPerson.grade > acc
+  ) {
+    acc = currentPerson.grade;
+  }
+  return acc;
+}, -Infinity);
 
 const fruitBasket = [
-  'banana',
-  'cherry',
-  'orange',
-  'apple',
-  'cherry',
-  'orange',
-  'apple',
-  'banana',
-  'cherry',
-  'orange',
-  'fig',
+  "banana",
+  "cherry",
+  "orange",
+  "apple",
+  "cherry",
+  "orange",
+  "apple",
+  "banana",
+  "cherry",
+  "orange",
+  "fig",
 ];
 
 /* 
@@ -52,6 +115,14 @@ Output:
 {banana: 2, cherry: 3, orange: 3, apple: 2, fig: 1}
 */
 
+const fruitCount = fruitBasket.reduce((acc, currentFruit) => {
+  if (typeof acc[currentFruit] != "undefined") {
+    acc[currentFruit] += 1;
+  } else {
+    acc[currentFruit] = 1;
+  }
+  return acc;
+}, {});
 /* 
 
 Use the fruitBasket array to create an array of array. Each array will contain two values name of fruit and number of times
@@ -61,6 +132,8 @@ Output:
 
 [['banana', 2], ['cherry', 3], ['orange', 3], ['apple', 2], ['fig', 1]]
 */
+const fruitsObjArr = Object.entries(fruitCount);
+console.log(fruitsObjArr);
 
 const data = [
   [1, 2, 3],
@@ -68,8 +141,10 @@ const data = [
   [7, 8, 9],
   [10, 11, 12],
 ];
-
 // Using reduce flat data array
+const flattenedData = data.reduce((acc, curVal) => {
+  return acc.concat(curVal);
+}, []);
 
 const dataTwo = [
   [1, 2, 3],
@@ -79,6 +154,9 @@ const dataTwo = [
 ];
 
 // Using reduce flat dataTwo array
+const flattenedDataTwo = dataTwo.reduce((acc, curVal) => {
+  return acc.concat(curVal);
+}, []);
 
 /*
 
@@ -88,7 +166,19 @@ Create these functions which accepts a number value and returns a number value:
   - `decrement` decrement 1 from the value 
   - `triple` triples the input 
   - `half` converts the value to half and return the integer value not decimal (use Math.round(21.5) => 21)
+
 */
+
+const increment = (num) => num + 1;
+
+const double = (num) => num * 2;
+
+const decrement = (num) => num - 1;
+
+const triple = (num) => num * 3;
+
+const half = (num) => parseInt(num / 2);
+
 
 let pipeline = [
   increment,
@@ -101,6 +191,13 @@ let pipeline = [
   increment,
 ];
 
+pipeline.reduce((acc, cv) => {
+  acc = cv(acc)
+  console.log(acc);
+  return acc;
+}, 3)
+
+/*
 /*
 Using the pipeline variable that contains the collection of functions, taking the initial value 3 find the output.
 
@@ -129,4 +226,12 @@ let pipeline2 = [
   triple,
 ];
 
-// Find the output using pipeline2 the initial value if 8
+
+//Find the output using pipeline2 the initial value if 8
+
+
+pipeline2.reduce((acc, cv) => {
+  acc = cv(acc)
+  console.log(acc);
+  return acc;
+}, 8)
